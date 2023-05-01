b308ac01949c42147a19497218ade0ed04d7c1c6
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-05-01 11:59:55 -0500

    test: ✅ Add tests for NautobotDatacenter create function

---
+++ nautobot_ssot_citrix_adm/tests/tests_models_nautobot.py
@@ -0,0 +1,29 @@
+"""Test the Nautobot CRUD functions for all DiffSync models."""
+from unittest.mock import MagicMock
+from diffsync import DiffSync
+from nautobot.dcim.models import Region, Site
+from nautobot.utilities.testing import TransactionTestCase
+from nautobot_ssot_citrix_adm.diffsync.models.nautobot import NautobotDatacenter
+
+
+class TestNautobotDatacenter(TransactionTestCase):
+    """Test the NautobotDatacenter class."""
+
+    def setUp(self):
+        """Configure shared objects."""
+        super().setUp()
+        self.diffsync = DiffSync()
+        self.diffsync.job = MagicMock()
+        self.diffsync.job.log_info = MagicMock()
+
+    def test_create(self):
+        """Validate the NautobotDatacenter create() method creates a Site."""
+        ids = {"name": "HQ", "region": "NY"}
+        attrs = {"latitude": "12.345", "longitude": "-67.89"}
+        result = NautobotDatacenter.create(self.diffsync, ids, attrs)
+        self.assertIsInstance(result, NautobotDatacenter)
+        site_obj = Site.objects.get(name=ids["name"])
+        ny_region = Region.objects.get(name=ids["region"])
+        self.assertEqual(site_obj.region, ny_region)
+        self.assertEqual(str(site_obj.latitude).rstrip("0"), attrs["latitude"])
+        self.assertEqual(str(site_obj.longitude).rstrip("0"), attrs["longitude"])
