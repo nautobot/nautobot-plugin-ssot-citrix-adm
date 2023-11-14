1e14a8442c189fd19a341f912f216ab2f63893c4
Justin Pettit <47164813+jmpettit@users.noreply.github.com>
2023-11-14 16:30:22 -0500

    Add tests for parsing helper functions

--- nautobot_ssot_citrix_adm/tests/test_adapters_citrix_adm.py
+++ nautobot_ssot_citrix_adm/tests/test_adapters_citrix_adm.py
@@ -3,7 +3,6 @@ import uuid
 from unittest.mock import MagicMock
 from diffsync.exceptions import ObjectNotFound
 from django.contrib.contenttypes.models import ContentType
-from netutils.ip import netmask_to_cidr
 from nautobot.dcim.models import Device, DeviceRole, DeviceType, Interface, Manufacturer, Site
 from nautobot.extras.models import CustomField, Job, JobResult, Status
 from nautobot.ipam.models import IPAddress
@@ -98,7 +97,7 @@ class TestCitrixAdmAdapterTestCase(TransactionTestCase):  # pylint: disable=too-
     def test_load_ports(self):
         """Test the Nautobot SSoT Citrix ADM load_ports() function."""
         self.citrix_adm.adm_device_map = ADM_DEVICE_MAP_FIXTURE
-        self.citrix_adm.get = MagicMock() 
+        self.citrix_adm.get = MagicMock()
         self.citrix_adm.get.side_effect = [ObjectNotFound, MagicMock(), ObjectNotFound, MagicMock()]
         self.citrix_adm.load_ports()
         expected_ports = {
@@ -124,7 +123,6 @@ class TestCitrixAdmAdapterTestCase(TransactionTestCase):  # pylint: disable=too-
         ]
         actual_addrs = [addr.get_unique_id() for addr in self.citrix_adm.get_all("address")]
         self.assertEqual(sorted(expected_addrs), sorted(actual_addrs))
-
 
     def test_label_imported_objects_not_found(self):
         """Validate the label_imported_objects() handling ObjectNotFound."""
