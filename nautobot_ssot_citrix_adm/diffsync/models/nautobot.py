dfda0dd167f896288fa05ed654415a32645228f1
Justin Drew <justin.drew@cox.com>
2024-10-31 15:15:12 -0400

    fix: 🐛 Update Prefix query for IPAddress parent to use net_contains for IPAddress host.

--- nautobot_ssot_citrix_adm/diffsync/models/nautobot.py
+++ nautobot_ssot_citrix_adm/diffsync/models/nautobot.py
@@ -249,7 +249,7 @@ class NautobotAddress(Address):
         """Create IP Address in Nautobot from NautobotAddress object."""
         new_ip = IPAddress(
             address=ids["address"],
-            parent=Prefix.objects.get(prefix=ids["prefix"]),
+            parent=Prefix.objects.filter(network__net_contains=ids["address"]).last(),
             status=Status.objects.get(name="Active"),
             namespace=(
                 Namespace.objects.get_or_create(name=attrs["tenant"])[0]
